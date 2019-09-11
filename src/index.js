f73ea275ef1e56938ea07b295e43744512a28868
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2019-09-11 18:17:34 -0300

    Mutations on The Client

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
+import { resolvers, typeDefs } from './graphql/resolvers';
 
 const httpLink = createHttpLink({
   uri: 'https://crwn-clothing.com'
@@ -21,25 +22,16 @@ const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   link: httpLink,
-  cache
+  cache,
+  typeDefs,
+  resolvers
 });
 
-client.query({
-  query: gql`
-    {
-      getCollectionsByTitle(title: "hats") {
-        id
-        title
-        items {
-          id
-          name
-          price
-          imageUrl
-        }
-      }
-    }
-  `
-}).then(res => console.log(res));
+client.writeData({
+  data: {
+    cartHidden: true
+  }
+})
 
 ReactDOM.render(
   <ApolloProvider client={client}>
