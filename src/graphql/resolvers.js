6375a696ec3e62d169b06a62c96b00fcb62778a4
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2019-09-13 16:09:17 -0300

    cartItem count with apollo

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,6 +1,6 @@
 import { gql } from 'apollo-boost';
 
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, getCartItemCount } from './cart.utils';
 
 export const typeDefs = gql`
   extend type Item {
@@ -16,7 +16,12 @@ const GET_CART_HIDDEN = gql`
   {
     cartHidden @client
   }
+`;
 
+const GET_ITEM_COUNT = gql`
+  {
+    itemCount @client
+  }
 `;
 
 const GET_CART_ITEMS = gql`
@@ -46,6 +51,11 @@ export const resolvers = {
       });
 
       const newCartItems = addItemToCart(cartItems, item);
+
+      cache.writeQuery({
+        query: GET_ITEM_COUNT,
+        data: { itemCount: getCartItemCount(newCartItems) }
+      });
 
       cache.writeQuery({
         query: GET_CART_ITEMS,
